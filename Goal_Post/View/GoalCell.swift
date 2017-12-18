7c965d8f479501577d8860594311800c3cd03285
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-18 10:43:22 -0800

    // GoalCell:  Added new Cocoa Touch file to View.  Created class GoalCell.  Deleted the standard plate function as not needed.  Added IBOutlets.  Created Function for Configure Cell.

---
+++ Goal_Post/View/GoalCell.swift
@@ -0,0 +1,62 @@
+//
+//  GoalCell.swift
+//  Goal_Post
+//
+//  Created by Donald Belliveau on 2017-12-18.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    /*
+     IBOutlets
+     */
+    
+    @IBOutlet weak var goalDescriptionLbl: UIStackView!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    /*
+     Functions
+     */
+    
+    
+    /* Configure Cell Function. */
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        
+    } // END Configure Cell
+    
+    
+} // END Class.
+
+
+// GoalCell:  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
