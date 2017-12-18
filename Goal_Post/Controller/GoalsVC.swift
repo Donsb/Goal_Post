07654af14d532c2c9acfd9a5f1999f3777fc8891
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-18 12:10:42 -0800

    // GoalsVC: Added TableView Extension with 3 requires Functions.  Set DataSource and Delegate to self.  Completed 3 TableView Functions.

--- Goal_Post/Controller/GoalsVC.swift
+++ Goal_Post/Controller/GoalsVC.swift
@@ -26,6 +26,8 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     } // END View Did Load.
     
     
@@ -45,8 +47,53 @@ class GoalsVC: UIViewController {
     
 } // END Class.
 
-// GoalsVC:  
+
+/*
+ Extensions
+ */
+
+// TableView Extension.
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    /* Number Of Sections Function. */
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    } // END Number Of Sections.
+    
+    
+    /* Number of Rows In Section Function. */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    } // END Number of Rows In Section.
+    
+    
+    /* Cell For Row At Function. */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat Salad Twice a week", type: "Short Term", goalProgressAmount: 2)
+        return cell
+    } // END Cell For Row At.
+    
+    
+} // END TableView Extension
+
+// GoalsVC: 
+
 // Goal (Core Data):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
