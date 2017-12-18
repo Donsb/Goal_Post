b618c9956e8b11470e9f917efe6abbeece2c356c
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-18 15:16:14 -0800

    UIButtonExt:  Added Swift File to Extensions folder called UIButtonExt.  Imported UIKit.  Created Extension for UIButton.  Created 2 Functions to set the colour if button is selected or not.

---
+++ Goal_Post/Extensions/UIButtonExt.swift
@@ -0,0 +1,33 @@
+//
+//  UIButtonExt.swift
+//  Goal_Post
+//
+//  Created by Donald Belliveau on 2017-12-18.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    /* Set Selected Colour Function. */
+    func setSelectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0, green: 0.5603182912, blue: 0, alpha: 1)
+    } // END Set Selected Colour.
+    
+    
+    /* Set Deselected Colour Function. */
+    func setDesectlectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.6980392157, green: 0.8666666667, blue: 0.6862745098, alpha: 1)
+    } // END Set Deselected Colour.
+    
+} // END Extension.
+
+
+/*
+ 
+ 
+ UIButtonExt:  
+ 
+ 
+ */
