cf0f8147334878ccfd42e74ebca5fd6a7e1e914d
Donald Belliveau <donaldsbelliveau@gmail.com>
2017-12-18 13:27:03 -0800

    // CreateGoalVC:  Created new VC storyboard for CreateGoal.  Added UI Elements.  Added Cocoa Touch file to Controller Folder.  Created class CreateGoalVC.

---
+++ Goal_Post/Controller/CreateGoalVC.swift
@@ -0,0 +1,35 @@
+//
+//  CreateGoalVC.swift
+//  Goal_Post
+//
+//  Created by Donald Belliveau on 2017-12-18.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+    /*
+     Functions.
+     */
+    
+    
+    /* View Did Load Function. */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    } // END View Did Load.
+    
+    
+    /* Did Receive Memory Warning Function. */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } // END Did Receive Memory Warning
+    
+    
+} // END Class.
+
+
+// CreateGoalVC:  Created new VC storyboard for CreateGoal.  Added UI Elements.  Added Cocoa Touch file to Controller Folder.  Created class CreateGoalVC.
